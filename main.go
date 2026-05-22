7cadbf40b73d11a413492ddd9d61ff01cb3c79a7
Arsenii Zhadanenko <arseniizh1@gmail.com>
2026-05-22 17:56:33 +0200

    Added global client

--- main.go
+++ main.go
@@ -1,24 +1,39 @@
+// Package main
 package main
 
 import (
 	"fmt"
+	"log"
+	"net/http"
+	"time"
 
 	binance "github.com/werastine/CryptoDifferenceAnalyser/BinanceMRKT"
-	hype "github.com/werastine/CryptoDifferenceAnalyser/HyperLiquid"
+	bybit "github.com/werastine/CryptoDifferenceAnalyser/ByBit"
+	hyperliquid "github.com/werastine/CryptoDifferenceAnalyser/HyperLiquid"
 )
 
 func main() {
 
-	CoinHL, err := hype.GetPriceHyperLiquid("LINK")
+	sharedClient := http.Client{Timeout: 5 * time.Second}
+
+	coinBB, err := bybit.GetByBitPrice(&sharedClient, "TON")
 	if err != nil {
-		fmt.Println("Found error ir GetPriceHyperLiquid func", err)
+		log.Printf("[ERROR] Bybit: %v", err)
 	}
 
-	CoinBN, err := binance.GetPriceBinance("LINK")
+	CoinHL, err := hyperliquid.GetPriceHyperLiquid(&sharedClient, "TON")
 	if err != nil {
-		fmt.Println("Found error ir GetPriceHyperLiquid func", err)
+		fmt.Println("Found error in GetPriceHyperLiquid func: ", err)
 	}
 
-	fmt.Println(CoinHL.Coin, ":", CoinHL.Price)
-	fmt.Println(CoinBN.Coin, ":", CoinBN.Price)
+	CoinBN, err := binance.GetPriceBinance(&sharedClient, "TON")
+	if err != nil {
+		fmt.Println("Found error in GetPriceBinance func:", err)
+	}
+
+	fmt.Println(coinBB.STExchange, coinBB.Symbol, ":", CoinHL.Price)
+	fmt.Println(CoinHL.STExchange, CoinHL.Coin, ":", CoinHL.Price)
+	fmt.Println(CoinBN.STExchange, CoinBN.Coin, ":", CoinBN.Price)
+
+	// fmt.Println("Difference is:", CoinBN.Price-CoinHL.Price)
 }
