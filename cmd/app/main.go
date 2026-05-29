36800775c2eef45fedcb5351ab1b3d4a080fea87
Arsenii Zhadanenko <arseniizh1@gmail.com>
2026-05-29 09:37:13 +0200

    Added graceful shutdown

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -2,9 +2,13 @@
 package main
 
 import (
+	"context"
+	"errors"
 	"log"
 	"net/http"
+	"os/signal"
 	"sync"
+	"syscall"
 	"time"
 
 	"github.com/werastine/CryptoDifferenceAnalyser/internal/api"
@@ -12,6 +16,9 @@ import (
 )
 
 func main() {
+	ctx, stop := signal.NotifyContext(context.Background(), syscall.SIGINT, syscall.SIGTERM)
+	defer stop()
+
 	client := &http.Client{Timeout: 5 * time.Second}
 	waitGroup := &sync.WaitGroup{}
 
@@ -19,8 +26,21 @@ func main() {
 	hdl := api.NewHandler(srv)
 	hdl.RegisterRoutes()
 
-	if err := http.ListenAndServe(":8081", nil); err != nil {
-		log.Println("[ERROR] connecting to a port:", err)
-	}
+	server := &http.Server{Addr: ":8081"}
 
+	go func() {
+		if err := server.ListenAndServe(); err != nil && !errors.Is(err, http.ErrServerClosed) {
+			log.Println("[ERROR] connecting to a port:", err)
+		}
+	}()
+
+	<-ctx.Done()
+	log.Println("[INFO] Recieved stop signal, making graceful shutdown...")
+	shutdownCtx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	if err := server.Shutdown(shutdownCtx); err != nil {
+		log.Println("[ERROR] calling for graceful shutdown", err)
+	}
+	log.Println("Server is gracefuly stopped")
 }
